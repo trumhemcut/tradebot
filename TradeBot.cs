ffab6a6083c075030ff76a3cc0d12f0cc8deddce
Phi Huynh <phi.huynhngoc@nashtechglobal.com>
2017-12-15 17:46:28 +0700

    Adding Binance price

--- TradeBot.cs
+++ TradeBot.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -22,7 +23,19 @@ namespace tradebot
 
         public async Task Execute()
         {
-            var result = await GetCoinPriceFromBittrex("ADA");
+            while (true)
+            {
+                var getBittrexPriceTask = GetCoinPriceFromBittrex("ADA");
+                var getBinancePriceTask = GetCoinPriceFromBinance("ADA");
+                await Task.WhenAll(getBittrexPriceTask, getBinancePriceTask);
+
+                Console.WriteLine($"Bittrex Price: {getBittrexPriceTask.Result.BidPrice} - " + 
+                                  $"Binance Price: {getBinancePriceTask.Result.BidPrice} - " +
+                                  $"Delta: {getBinancePriceTask.Result.BidPrice - getBittrexPriceTask.Result.BidPrice}");
+
+                Thread.Sleep(1000);
+
+            }
         }
 
         public async Task<CoinPrice> GetCoinPriceFromBittrex(string coin)
@@ -31,7 +44,8 @@ namespace tradebot
             {
                 string result = await client.GetStringAsync($"https://bittrex.com/api/v1.1/public/getticker?market=BTC-{coin}");
                 dynamic d = JsonConvert.DeserializeObject(result);
-                return new CoinPrice{
+                return new CoinPrice
+                {
                     Coin = coin,
                     LastPrice = d.result.Last,
                     BidPrice = d.result.Bid,
@@ -42,16 +56,18 @@ namespace tradebot
         }
 
         // Reference to https://www.binance.com/restapipub.html#user-content-market-data-endpoints
-        public async Task<CoinPrice> GetCoinPriceFromBinance(string coin){
+        public async Task<CoinPrice> GetCoinPriceFromBinance(string coin)
+        {
             using (var client = new HttpClient())
             {
-                string result = await client.GetStringAsync($"https://api.binance.com/api/v1/depth?symbol=ADABTC");
+                string result = await client.GetStringAsync($"https://api.binance.com/api/v1/depth?symbol={coin}BTC&limit=5");
                 dynamic d = JsonConvert.DeserializeObject(result);
-                return new CoinPrice{
+                return new CoinPrice
+                {
                     Coin = coin,
-                    LastPrice = d.result.Last,
-                    BidPrice = d.result.Bid,
-                    AskPrice = d.result.Ask,
+                    LastPrice = d.bids[0][0],
+                    BidPrice = d.bids[0][0],
+                    AskPrice = d.asks[0][0],
                     RetrivalTime = DateTime.Now
                 };
             }
