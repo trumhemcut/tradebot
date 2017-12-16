94d7a59797eb7ed2dd783484d6bd2b135358aa8a
Phi Huynh <phi.huynhngoc@nashtechglobal.com>
2017-12-16 22:23:01 +0700

    Add Test project

---
+++ tests/TradeBot_CaculateProfitShould.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class TradeBot_CaculateProfitShould
+    {
+        public TradeBot_CaculateProfitShould()
+        {
+            
+        }
+        
+        [Fact]
+        public void ReturnCorrectPrice()
+        {
+
+        }
+    }
+}
