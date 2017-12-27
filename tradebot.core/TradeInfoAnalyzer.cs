d1257eb3728d37a308d451bd57e9cda71b0f79d8
Phi Huynh <phi.huynhngoc@nashtechglobal.com>
2017-12-27 23:50:02 +0700

    Fix decimal

--- tradebot.core/TradeInfoAnalyzer.cs
+++ tradebot.core/TradeInfoAnalyzer.cs
@@ -25,11 +25,11 @@ namespace tradebot.core
             var deltaBidBid = this._sellAccount.TradeCoin.CoinPrice.BidPrice -
                               this._buyAccount.TradeCoin.CoinPrice.BidPrice;
 
-            var coinQtyAtBuy = quantity;//* (1 + this._buyAccount.TradingFee / 100);
+            var coinQtyAtBuy = quantity* (1.00000000M + this._buyAccount.TradingFee / 100.00000000M);
             var coinQtyAtSell = quantity;
 
-            var bitcoinQuantityAtSell = this._sellAccount.CurrentBidPrice * coinQtyAtSell * (1 - this._sellAccount.TradingFee / 100);
-            var bitcoinQuantityAtBuy = this._buyAccount.CurrentAskPrice * coinQtyAtBuy * (1 + this._buyAccount.TradingFee / 100);
+            var bitcoinQuantityAtSell = this._sellAccount.CurrentBidPrice * coinQtyAtSell * (1.00000000M - this._sellAccount.TradingFee / 100.00000000M);
+            var bitcoinQuantityAtBuy = this._buyAccount.CurrentAskPrice * coinQtyAtBuy;
 
             // Check coin balances at both side to make sure it's ok to order
             if (bitcoinQuantityAtBuy >= this._buyAccount.Bitcoin.Balance)
